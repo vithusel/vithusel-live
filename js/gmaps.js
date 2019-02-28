140f67ba4abf7526a72ec691d5ca164e40bde583
vithusel <31311292+vithusel@users.noreply.github.com>
2019-02-28 16:15:09 +0000

    compress EVERYTHING!!!!

--- js/gmaps.js
+++ js/gmaps.js
@@ -1,10 +1,1 @@
-// Initialize and add the map
-function initMap() {
-  // The location of Uluru
-  var croydon = {lat: 51.3762, lng: -0.098234};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 13, center: croydon});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: croydon, map: map});
-}
+function initMap(){var b={lat:51.3762,lng:-0.098234};var c=new google.maps.Map(document.getElementById("map"),{zoom:13,center:b});var a=new google.maps.Marker({position:b,map:c})};
